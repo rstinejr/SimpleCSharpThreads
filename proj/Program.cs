bc2ab1c37972d2017f4ffcb92ecf0953a30ece7d
R.H. Stine <bob@waltonstine.net>
2016-12-17 10:35:05 -0500

    Fiddle with thread basics.

--- proj/Program.cs
+++ proj/Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Reflection;
+using System.Threading;
 
 namespace Waltonstine.Example.CSharp.Threads
 {
-    public class Program
+    class Worker
+    {
+        private Mutex mutx;
+
+        public Worker(Mutex m)
+        {
+            mutx = m;
+        }
+
+        public bool Quit { get; set; }
+
+        public void DoSomeWork()
+        {
+            Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} started.");
+
+            mutx.WaitOne();
+
+            try
+            {
+                if (Quit)
+                {
+                    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} done. Throw an exception.");
+                    throw new Exception("boo");
+                }
+
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} has mutex. Do work, release mutex, exit.");
+
+                // work work work
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"Exception caught in thread {Thread.CurrentThread.ManagedThreadId}");
+            }
+            finally
+            {
+                mutx.ReleaseMutex();
+            }
+
+            return;
+        }
+    }
+
+    public class MainClass
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Main thread {Thread.CurrentThread.ManagedThreadId}, create a worker thread.");
+            Mutex mutx = new Mutex();
+            mutx.WaitOne();
+
+            Worker worker = new Worker(mutx);
+            Thread t = new Thread(new ThreadStart(worker.DoSomeWork));
+
+            Console.WriteLine("Start thread");
+            t.Start();
+
+            Thread.Sleep(10);
+            Console.Write("Press 'x' to throw exception in thread, any other key to continue: ");
+            ConsoleKeyInfo ki = Console.ReadKey();
+            Console.WriteLine();
+            if (ki.KeyChar == 'x')
+            {
+                worker.Quit = true;
+            }
+            mutx.ReleaseMutex();
+            
+            t.Join();
+
+            Console.WriteLine("Thread joined. Done.");
         }
     }
 }
